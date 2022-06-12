5e98b9700107830ac2269bb81feb981d9bf3345d
Haruki Okada <ocadaruma@gmail.com>
2022-06-13 01:26:57 +0900

    add a script to make a release

---
+++ release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# Script to make a new release.
+# Upon successful CI, you need to execute maven-publish.sh next.
+
+cd $(dirname $0)
+
+version="$1"
+if [ -z "$version" ]; then
+    echo "Usage: $0 VERSION" >&2
+    exit 1
+fi
+
+if [ $(git tag | grep "^v$version\$" | wc -l) -ne 0 ]; then
+    echo "$version already released"
+    exit 1
+fi
+
+sed -i "" -e "s/^version=.*$/version=$version/" ../gradle.properties
+
+git add gradle.properties
+git commit -m "Release $version"
+
+git push origin master
+
+tag="v$version"
+git tag $tag
+git push origin $tag
